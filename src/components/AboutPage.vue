8d4bca2a22764568cf77265478b9645c20ac4758
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-02-10 17:58:59 -0500

    build scaffolding

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="p-10">
+    <h1 class="text-2xl font-medium">About us</h1>
+    <p class="text-lg">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare
+      vestibulum massa, vel gravida ante efficitur in. Sed euismod velit id
+      justo hendrerit, a tristique ligula faucibus. Sed varius ex ac mauris
+      facilisis, id consequat metus tristique. Aliquam erat volutpat.
+      Vivamus placerat semper orci, id bibendum lacus dictum at.
+    </p>
+  </div>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="flex items-center justify-end space-x-4 p-6">
+    <router-link to="/" class="text-lg mr-4">Home</router-link>
+    <router-link to="/about" class="mr-4">About us</router-link>
+    <router-link to="/gallery" class="mr-4">Gallery</router-link>
+    <router-link to="/contact" class="mr-4">Contact Us</router-link>
+  </nav>
+</template>
