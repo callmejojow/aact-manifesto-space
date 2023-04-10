4ff78ee2c5eedc3bfb606077e420f8d557a38ad1
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-04-10 15:24:10 -0400

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col justify-end md:justify-start min-h-screen text-white/90 relative">
+  <div class="flex flex-col justify-end md:justify-start min-h-screen text-white/90 relative max-w-screen overflow-hidden ">
     <span class="sr-only">Current Curation</span>
     <img src="@/assets/curation1.png" class="scale-x-150 sm:scale-x-120 md:scale-x-100 z-0 w-screen min-h-screen lg:w-4/5 xl:w-3/5 self-end" alt="Curation Cover"/>
     <div class="absolute px-16 mb-40 md:mb-0 mt-0 md:mt-96">
