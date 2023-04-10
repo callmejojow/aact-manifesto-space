bc56409682b546e29abdc8f0140631b03054cabf
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-04-10 15:43:48 -0400

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col justify-end md:justify-start min-h-screen text-white/90 relative max-w-screen overflow-hidden ">
     <span class="sr-only">Current Curation</span>
-    <img src="@/assets/curation1.png" class="scale-x-150 sm:scale-x-120 md:scale-x-100 z-0 w-screen min-h-screen lg:w-4/5 xl:w-3/5 self-end" alt="Curation Cover"/>
-    <div class="absolute px-16 mb-40 md:mb-0 mt-0 md:mt-96">
+    <img src="@/assets/curation1.png" class="z-0 scale-125 mt-10 md:mt-0 md:scale-100 min-h-screen lg:w-4/5 xl:w-3/5 self-end" alt="Curation Cover"/>
+    <div class="absolute px-16 mb-64 md:mb-0 mt-0 md:mt-96">
       <h1 class="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-semibold tracking-wide">The Bitten Peach</h1>
       <p class="text-lg sm:text-2xl md:text-3xl lg:text-4xl tracking-wider font-medium my-4">
         Decolonizing Queer Asians
