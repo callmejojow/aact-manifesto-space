2e95ebe228970c17b90ab358e92bbf95673eb3e6
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-04-10 15:33:44 -0400

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,10 +4,10 @@
     <img src="@/assets/curation1.png" class="scale-x-150 sm:scale-x-120 md:scale-x-100 z-0 w-screen min-h-screen lg:w-4/5 xl:w-3/5 self-end" alt="Curation Cover"/>
     <div class="absolute px-16 mb-40 md:mb-0 mt-0 md:mt-96">
       <h1 class="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-semibold tracking-wide">The Bitten Peach</h1>
-      <p class="text-lg sm:text-2xl md:text-3xl lg:text-4xl leading-8 tracking-wider font-medium mt-2">
+      <p class="text-lg sm:text-2xl md:text-3xl lg:text-4xl tracking-wider font-medium mt-2">
         Decolonizing Queer Asians
       </p>
-      <a type="button" class="mt-4 border border-white/60 px-2.5 py-2 text-white/60 hover:border/white hover:text-white">Explore Now</a>
+      <a type="button" class="mt-8 border border-white/60 px-2.5 py-2 text-white/60 hover:border/white hover:text-white">Explore Now</a>
     </div>
   </div>
 </template>
