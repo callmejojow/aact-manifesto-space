b2d58f7b4bc917d4115026e5adaaec9b7a6d2bf0
Michael <xywang02@gmail.com>
2023-04-20 15:27:24 -0400

    misc: removed redundant edits

--- src/components/ArtistPage.vue
+++ src/components/ArtistPage.vue
@@ -10,23 +10,3 @@
     </p>
   </div>
 </template>
-
-<script>
-export default {
-  name: "ArtistPage",
-  // todo: different visuals for different artist categories?
-  // todo: best way to store artist data?
-
-  data() {
-    return {
-      artists: [
-        {
-          name: "Artist 1",
-          image: "https://picsum.photos/200/300",
-          bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare vestibulum massa, vel gravida ante efficitur in. Sed euismod velit id justo hendrerit, a tristique ligula faucibus. Sed varius ex ac mauris facilisis, id consequat metus tristique. Aliquam erat volutpat. Vivamus placerat semper orci, id bibendum lacus dictum at."
-        },
-      ]
-    }
-  },
-}
-</script>
