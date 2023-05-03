fd7fea98fd8319ecbe75684768e2de757a234f0f
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-05-03 01:06:54 -0400

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -30,20 +30,22 @@ const updateBgImage = () => {
        <img src="@/assets/manifesto-logo.png" class="w-6 h-6" alt="Website Logo" />
       </a> 
       <p class="text-white tracking-widest ml-6">MANIFESTO</p>
-      <div class="absolute right-4 text-white z-40">
+      <div class="absolute right-0 text-white z-40">
       <NavDropdown />
       </div>
     </div>
   
   <div class="flex flex-col items-start justify-start text-white/90 max-w-screen">
     <span class="sr-only">Current Curation</span>
-    <div class="px-16 mt-96">
-      <h1 class="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-semibold tracking-wide">The Bitten Peach</h1>
+    <kinesis-container>
+    <div class="pl-6 sm:pl-12 md:pl-16 mt-96">
+      <h1 class="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-semibold tracking-wide"><kinesis-element :strength="30" transformOrigin="100% 500%" type="depth">The Bitten Peach</kinesis-element></h1>
       <p class="text-lg sm:text-2xl md:text-3xl lg:text-4xl tracking-wider font-medium my-4">
-        Decolonizing Queer Asians
+        <kinesis-element :strength="70" transformOrigin="100% 500%" type="depth">Decolonizing Queer Asians</kinesis-element>
       </p>
-      <a href="/exhibitions#about" type="button" class="mt-6 border border-white/60 px-2.5 py-2 text-white/60 hover:border/white hover:text-white bg-white/10">Explore Now</a>
+      <a href="/exhibitions#about" type="button" class="z-20 mt-6 border border-white/60 px-2.5 py-2 text-white/60 hover:border/white hover:text-white bg-white/10">Explore Now</a>
     </div>
+    </kinesis-container>
   </div>
   </main>
 </template>
