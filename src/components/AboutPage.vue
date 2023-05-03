6e24d8733b82f6df2e11b4ebc8e82deebd80f370
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-05-03 16:41:53 -0400

    add about team page

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,12 +1,91 @@
+<script setup>
+import BreadCrumbs from '@/components/BreadCrumbs.vue'
+import NavDropdown from '@/components/NavDropdown.vue'
+</script>
 <template>
-  <div class="p-10">
-    <h1 class="text-2xl font-medium">About Us</h1>
-    <p class="text-lg">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare
-      vestibulum massa, vel gravida ante efficitur in. Sed euismod velit id
-      justo hendrerit, a tristique ligula faucibus. Sed varius ex ac mauris
-      facilisis, id consequat metus tristique. Aliquam erat volutpat.
-      Vivamus placerat semper orci, id bibendum lacus dictum at.
-    </p>
-  </div>
+    <div class="scroll-smooth font-open max-h-screen">
+        <div class="sticky top-0 z-20 bg-ivory">
+            <div class="flex items-center justify-start lg:justify-center py-4 px-4 border-b border-stone-200/80">
+                <a href="/">
+                    <img src="@/assets/manifesto-logo-black.svg" class="w-6 h-6" alt="Website Logo" />
+                </a>
+                <p class="text-black tracking-widest ml-6">MANIFESTO</p>
+                <div class="absolute right-4 text-ivory z-40">
+                    <NavDropdown />
+                </div>
+            </div>
+            <!-- Generic Breadcrumbs -->
+            <div class="hidden lg:flex lg:justify-start lg:items-center lg:p-4">
+                <BreadCrumbs />
+                <span class="ml-4 text-sm text-stone-400/80 capitalize">
+                    meet the team
+                </span>
+            </div>
+            <div class="bg-ivory container p-4">
+              <h1 class="capitalize font-extrabold text-2xl md:text-3xl lg:text-5xl text-black/80">our team memebers</h1>
+              <div class="py-6 grid md:grid-cols-2 gap-16 justify-center mx-auto px-6 mg:px-12 lg:px-16">
+                <div class="flex justify-start items-center gap-4">
+                  <div>
+                    <img src="@/assets/avatar.png" class="w-24 lg:w-28 w-24 lg:h-28 rounded-full object-contain" />
+                  </div>
+                  <div>
+                    <h2 class="capitalize font-bold text-xl md:text-2xl xl:text-3xl text-black/80">team leader name</h2>
+                    <p class="font-semibold text-base xl:text-lg">Role/Position (Tenure)</p>
+                    <p class="font-semibold text-base xl:text-lg">Visit my socials: @intagram or whatever</p>
+                  </div>
+                </div>
+                <div class="flex justify-start items-center gap-4">
+                  <div>
+                    <img src="@/assets/avatar.png" class="w-24 lg:w-28 w-24 lg:h-28 rounded-full object-contain" />
+                  </div>
+                  <div>
+                    <h2 class="capitalize font-bold text-xl md:text-2xl xl:text-3xl text-black/80">team leader name</h2>
+                    <p class="font-semibold text-base xl:text-lg">Role/Position (Tenure)</p>
+                    <p class="font-semibold text-base xl:text-lg">Visit my socials: @intagram or whatever</p>
+                  </div>
+                </div>
+                <div class="flex justify-start items-center gap-4">
+                  <div>
+                    <img src="@/assets/avatar.png" class="w-24 lg:w-28 w-24 lg:h-28 rounded-full object-contain" />
+                  </div>
+                  <div>
+                    <h2 class="capitalize font-bold text-xl md:text-2xl xl:text-3xl text-black/80">team leader name</h2>
+                    <p class="font-semibold text-base xl:text-lg">Role/Position (Tenure)</p>
+                    <p class="font-semibold text-base xl:text-lg">Visit my socials: @intagram or whatever</p>
+                  </div>
+                </div>
+                <div class="flex justify-start items-center gap-4">
+                  <div>
+                    <img src="@/assets/avatar.png" class="w-24 lg:w-28 w-24 lg:h-28 rounded-full object-contain" />
+                  </div>
+                  <div>
+                    <h2 class="capitalize font-bold text-xl md:text-2xl xl:text-3xl text-black/80">team leader name</h2>
+                    <p class="font-semibold text-base xl:text-lg">Role/Position (Tenure)</p>
+                    <p class="font-semibold text-base xl:text-lg">Visit my socials: @intagram or whatever</p>
+                  </div>
+                </div>
+                <div class="flex justify-start items-center gap-4">
+                  <div>
+                    <img src="@/assets/avatar.png" class="w-24 lg:w-28 w-24 lg:h-28 rounded-full object-contain" />
+                  </div>
+                  <div>
+                    <h2 class="capitalize font-bold text-xl md:text-2xl xl:text-3xl text-black/80">team leader name</h2>
+                    <p class="font-semibold text-base xl:text-lg">Role/Position (Tenure)</p>
+                    <p class="font-semibold text-base xl:text-lg">Visit my socials: @intagram or whatever</p>
+                  </div>
+                </div>
+                <div class="flex justify-start items-center gap-4">
+                  <div>
+                    <img src="@/assets/avatar.png" class="w-24 lg:w-28 w-24 lg:h-28 rounded-full object-contain" />
+                  </div>
+                  <div>
+                    <h2 class="capitalize font-bold text-xl md:text-2xl xl:text-3xl text-black/80">team leader name</h2>
+                    <p class="font-semibold text-base xl:text-lg">Role/Position (Tenure)</p>
+                    <p class="font-semibold text-base xl:text-lg">Visit my socials: @intagram or whatever</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
 </template>
