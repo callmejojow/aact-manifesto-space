9c99eb4234f687f114f2cb4b8dc089cadd7b3a01
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-05-03 20:49:17 -0400

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,8 @@
 <script setup>
 import NavDropdown from '@/components/NavDropdown.vue'
-import { ref, onMounted, onUnmounted } from 'vue'
-import coverMobile from '@/assets/cover2.png'
-import coverDesktop from '@/assets/cover1.png'
-
+import { ref, onMounted } from 'vue'
 const buttonOpacity = ref(0);
+import bgImg from '@/assets/curation-topic1.webp'
 
 function navigateWithFadeOut(url) {
   const body = document.querySelector("body");
@@ -15,27 +13,12 @@ function navigateWithFadeOut(url) {
   }, 500); // Wait for 500ms (the duration of the fade-out animation) before navigating
 }
 
-const bgImage = ref(coverMobile)
-
-const updateBgImage = () => {
-      if (window.innerWidth >= 768) {
-        bgImage.value = coverDesktop
-      } else {
-        bgImage.value = coverMobile
-      }
-    }
 onMounted(() => {
-  updateBgImage()
-  window.addEventListener('resize', updateBgImage)
-
   setTimeout(() => {
     buttonOpacity.value = 1;
   }, 1600);
 })
 
-onUnmounted(() => {
-  window.removeEventListener('resize', updateBgImage)
-})
 </script>
 <style>
 .fade-out {
@@ -51,15 +34,15 @@ onUnmounted(() => {
   }
 }
 
-div.slide-left {
+/*div.slide-left {
   width:100%;
   overflow:hidden;
 }
 div.slide-left p {
   animation: slide-left 5s;
-}
+}*/
 
-@keyframes slide-left {
+/*@keyframes slide-left {
   from {
     margin-left: 100%;
     width: 300%; 
@@ -69,7 +52,7 @@ div.slide-left p {
     margin-left: 0%;
     width: 100%;
   }
-}
+}*/
 
 @keyframes fadeIn {
   from {
@@ -81,7 +64,7 @@ div.slide-left p {
 }
 </style>
 <template>
-  <main class="font-open overflow-hidden h-screen bg-cover bg-right-top" :style="`background-image: url(${bgImage});`">
+  <main class="font-open overflow-hidden h-screen bg-cover bg-center lg:bg-right-top" :style="`background-image: url(${bgImg});`">
     <div class="flex justify-start md:justify-center items-center py-4 px-4 md:px-0">
       <a href="/">
        <img src="@/assets/manifesto-logo.png" class="w-6 h-6" alt="Website Logo" />
@@ -94,18 +77,16 @@ div.slide-left p {
   
   <div class="slide-left flex flex-col items-start justify-start text-ivory/90 max-w-screen">
     <span class="sr-only">Current Curation</span>
-    <kinesis-container>
     <div class="pl-6 sm:pl-12 md:pl-16 mt-96">
-      <p class="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-semibold tracking-wide"><kinesis-element :strength="30" transformOrigin="100% 500%" type="depth">The Bitten Peach</kinesis-element></p>
-      <p class="text-lg sm:text-2xl md:text-3xl lg:text-4xl tracking-wider font-medium my-4">
-        <kinesis-element :strength="70" transformOrigin="100% 500%" type="depth">Decolonizing Queer Asians</kinesis-element>
+      <p class="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-semibold tracking-wide" :style="{ opacity: buttonOpacity, transition: 'opacity 100ms ease-in' }">The Bitten Peach</p>
+      <p class="text-lg sm:text-2xl md:text-3xl lg:text-4xl tracking-wider font-medium my-4" :style="{ opacity: buttonOpacity, transition: 'opacity 400ms ease-in' }">
+        Decolonizing Queer Asians
       </p>
       <button @click="navigateWithFadeOut('/exhibitions#about')" 
               type="button"
               :style="{ opacity: buttonOpacity, transition: 'opacity 600ms ease-in' }"
               class="z-20 mt-6 border border-ivory/60 px-2.5 py-2 text-ivory/60 hover:border/ivory hover:text-ivory bg-ivory/10">Explore Now</button>
     </div>
-    </kinesis-container>
   </div>
   </main>
 </template>
