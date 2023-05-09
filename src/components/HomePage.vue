23f7125adcaebdb09dfa35a06e424ec011a83b7c
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-05-08 23:29:54 -0400

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -66,7 +66,7 @@ div.slide-left p {
 </style>
 <template>
     <main class="min-h-screen max-h-full overflow-hidden relative bg-cover bg-center bg-fixed lg:bg-right-top" :style="`background-image: url(${bgImg});`">
-        <div class="h-[calc(100vh+6rem)] md:h-[calc(100vh+4rem)] w-screen m-0 font-open bg-black/20 flex flex-col justify-between">
+        <div class="h-[calc(100vh+6rem)] md:h-[calc(100vh+4rem)] w-screen m-0 bg-black/20 flex flex-col justify-between">
             <div class="flex justify-start md:justify-center items-center pt-4 md:pt-6 lg:pt-4 px-6 md:px-12 lg:px-0">
                 <a href="/">
                     <img src="@/assets/manifesto-logo-white.svg" class="w-2/3 opacity-90" alt="Website Logo" />
@@ -77,12 +77,12 @@ div.slide-left p {
             </div>
             <div class="self-start flex items-center justify-center text-ivory max-w-screen">
                 <span class="sr-only">Current Curation</span>
-                <div class="pl-12 md:pl-24 lg:pl-36">
-                    <p class="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-semibold tracking-wide" :style="{ opacity: buttonOpacity, transition: 'opacity 100ms ease-in' }">The Bitten Peach</p>
-                    <p class="text-lg sm:text-2xl md:text-3xl lg:text-4xl tracking-wider font-medium my-4" :style="{ opacity: buttonOpacity, transition: 'opacity 150ms ease-in' }">
+                <div class="pl-6 md:pl-12 lg:pl-24">
+                    <p class="text-2xl md:text-4xl lg:text-5xl font-medium tracking-wide" :style="{ opacity: buttonOpacity, transition: 'opacity 100ms ease-in' }">The Bitten Peach:</p>
+                    <p class="text-2xl md:text-4xl lg:text-5xl font-medium tracking-wide" :style="{ opacity: buttonOpacity, transition: 'opacity 150ms ease-in' }">
                         Decolonizing Queer Asians
                     </p>
-                    <button @click="navigateWithFadeOut('/exhibitions')" type="button" :style="{ opacity: buttonOpacity, transition: 'opacity 350ms ease-in' }" class="z-20 mt-6 border border-ivory/60 px-2.5 py-2 text-ivory/60 hover:border/ivory hover:text-ivory bg-ivory/10">Explore Now</button>
+                    <button @click="navigateWithFadeOut('/exhibitions')" type="button" :style="{ opacity: buttonOpacity, transition: 'opacity 350ms ease-in' }" class="z-20 mt-6 md:mt-9 lg:mt-12 border border-ivory/60 px-6 py-2 md:py-4 text-ivory/60 hover:border/ivory hover:text-ivory bg-ivory/10 md:text-base text-small">Explore Now</button>
                 </div>
             </div>
             <FooterComponent class="text-ivory/70 mb-2" />
