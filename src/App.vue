0cb5bffd0df50112c1a1d4491e20283657dac8c0
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-05-15 10:27:28 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="min-w-full max-w-screen lg:overflow-x-hidden lg:m-0">
+  <div id="app" class="min-w-full max-w-screen lg:overflow-x-hidden lg:m-0 bg-ivory">
     <router-view />
   </div>
 </template>
