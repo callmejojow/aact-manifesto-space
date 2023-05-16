ea4eacc2e6ad73897b61b768295234ed9364ca19
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-05-16 12:14:16 -0400

    change avatar and add erin

--- src/team.js
+++ src/team.js
@@ -2,43 +2,43 @@ export const team = [
   {
     name: 'Shengyu Cai',
     role: 'Curatorial Lead',
-    imageUrl:'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    imageUrl:require('@/assets/members/sheng.png'),
     bio:'Sheng is an artist, architectural designer, and curator. He is pursing his masters in architecture at Yale University, New Haven, USA. Sheng aspires to decouple architecture with colonial and homogenizing influences and explore various mediums of spatial art that intersects with queer theory. With rich exhibition design experiences under his belt, Sheng is passionate about contributing to AACT’s curatorial agenda.'
   },
   {
-    name: 'Zengcong Lai',
+    name: 'Zeng Lai',
     role: 'Digital Design Lead',
-    imageUrl:'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    imageUrl:require('@/assets/members/zeng.png'),
     bio:'Zeng Lai is a product designer, content creator, and entrepreneur with an architecture and finance background. As a first-generation queer Chinese Canadian, he has a keen awareness of the challenges that arise from navigating ideological and cultural divides. This has fueled his fascination with exploring the concepts of dualities and in-betweenness. With extensive experience in both public and private sectors, he is committed to leveraging technology and design to create solutions for the community.'
   },
   {
     name: 'Jolie Wang',
     role: 'Development Lead',
-    imageUrl:'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    imageUrl:require('@/assets/members/jolie.png'),
     bio:'Jolie Wang is a highly skilled full-stack developer and product designer with a fervent passion for crafting exceptional user experiences. As a freelance illustrator and web content consultant, Jolie leverages her comprehensive expertise in programming, art, and design to help clients bring their innovative ideas to fruition. Dedicated to promoting the advancement of women in technology, Jolie actively employs her talents to support and empower her female peers within the industry.'
+  },
+  {
+    name: 'Chevonne Xue',
+    role: 'Visual Design Lead',
+    imageUrl:require('@/assets/members/chevonne.png'),
+    bio:'Chevonne is a sociologist-designer focusing on user experience and product development. Her background in sociology, communication studies, and design background allows her to create aesthetic and feasible design solutions that link revenue to creating social benefits.'
   },
   {
     name: 'Emily Gong',
     role: 'Community Partnerships Lead',
-    imageUrl:'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    imageUrl:require('@/assets/members/emily.png'),
     bio:'Emily Gong (she/her/they) is a Chinese Canadian artist, researcher, and entrepreneur. Emily holds a Bachelor of Fine Arts Honours degree from Queen’s University and a Master of Science degree from the University of Oxford. Her masters research is focused on the comparative studies of the global art markets from the perspective of empowering artists in this system. Emily has written for the Barbican Centre and exhibited in the Shenzhen-Hong Kong Biennale of Urbanism.'
   },
   {
     name: 'Michael Wang',
     role: 'Community Operations Lead',
-    imageUrl:'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    imageUrl:require('@/assets/members/michael.png'),
     bio:'Michael Wang (he/him/his), Ph.D., is a vision scientist and tech enthusiast. His deep fascination about how humans perceive the world around them has led him to explore how personal and lived experiences are represented in various forms of art. To this end, Michael applies his knowledge in scientific research and modern technology to collaborate with local artists in Toronto to transform the creation and exhibition of arts.'
-  },
-  {
-    name: 'Chevonne Xue',
-    role: 'Visual Design Lead',
-    imageUrl:'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    bio:'Chevonne is a sociologist-designer focusing on user experience and product development. Her background in sociology, communication studies, and design background allows her to create aesthetic and feasible design solutions that link revenue to creating social benefits.'
   },
   {
     name: 'Erin Rooney',
     role: 'Lead Editor',
-    imageUrl:'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+    imageUrl:require('@/assets/members/erin.png'),
     bio:'Erin is a Data Management Coordinator with an MI from the University of Toronto. She has a passion for the written word and socio-cultural anthropology. She spent her formative years abroad, primarily in Asia, where she gained a lasting appreciation for Asian culture. Erin holds a BA from Queen’s University where she studied her lifelong love of English Literature and Language as well as History. She pursued her interest in Chinese studies at Queen’s University by enrolling in Chinese Language courses where she participated in departmental activities and events that strengthened her appreciation and understanding of Chinese culture. Erin is fascinated by the role that storytelling plays within culture and society and enjoys spending her free time pursuing her creative interests in reading and writing. She is an advocate for celebrating cultural diversity and tolerance through the art of storytelling in various mediums, joining AACT to promote cultural awareness and diversity through the creative arts.'
   },
   //more members here
