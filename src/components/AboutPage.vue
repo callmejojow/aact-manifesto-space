ddaebbafd03fde9522dc415cd6908954e7313bbd
Michael <xywang02@gmail.com>
2023-05-20 08:26:54 -0400

    fix: updated team social

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -82,7 +82,8 @@ function onIntersection(entry, index) {
 <!--                            <img class="aspect-[1/1] rounded object-cover" :src="member.imageUrl" alt="Profile Image" />-->
                             <h3 class="mt-4 lg:mt-6 lg:text-2xl font-bold text-gray-900">{{ member.name }}</h3>
                             <p class="lg:leading-7 lg:mt-1 text-base text-gray-600">{{ member.role }}</p>
-                            <p class="text-base mt-3 leading-6">{{ member.bio }}</p>
+                          <p class="text-base mt-3 leading-6">{{ member.bio }}</p>
+                          <div class="text-base mt-3 leading-6" v-html="member.social"></div>
                         </li>
                     </ul>
                 </div>
--- src/team.js
+++ src/team.js
@@ -3,43 +3,60 @@ export const team = [
     name: 'Shengyu Cai',
     role: 'Curation',
     imageUrl:require('@/assets/members/sheng.png'),
-    bio:'Sheng is an artist, architectural designer, and curator. He is pursing his masters in architecture at Yale University, New Haven, USA. Sheng aspires to decouple architecture with colonial and homogenizing influences and explore various mediums of spatial art that intersects with queer theory. With rich exhibition design experiences under his belt, Sheng is passionate about contributing to AACT’s curatorial agenda.'
+    bio:'Sheng is an artist, architectural designer, and curator. He is pursing his masters in architecture at Yale University, New Haven, USA. Sheng aspires to decouple architecture with colonial and homogenizing influences and explore various mediums of spatial art that intersects with queer theory. With rich exhibition design experiences under his belt, Sheng is passionate about contributing to AACT’s curatorial agenda.',
+    social: "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.instagram.com/studiosheng/\">@studiosheng</a>," +
+        "&nbsp;" +
+        // "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.instagram.com/cai.sheng.yu/\">@cai.sheng.yu</a>," +
+        // "&nbsp;" +
+        "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.linkedin.com/in/shengyu-cai-50896a98/\">LinkedIn</a>"
   },
   {
     name: 'Zeng Lai',
     role: 'Digital Design',
     imageUrl:require('@/assets/members/zeng.png'),
-    bio:'Zeng Lai is a product designer, content creator, and entrepreneur with an architecture and finance background. As a first-generation queer Chinese Canadian, he has a keen awareness of the challenges that arise from navigating ideological and cultural divides. This has fueled his fascination with exploring the concepts of dualities and in-betweenness. With extensive experience in both public and private sectors, he is committed to leveraging technology and design to create solutions for the community.'
+    bio:'Zeng Lai is a product designer, content creator, and entrepreneur with an architecture and finance background. As a first-generation queer Chinese Canadian, he has a keen awareness of the challenges that arise from navigating ideological and cultural divides. This has fueled his fascination with exploring the concepts of dualities and in-betweenness. With extensive experience in both public and private sectors, he is committed to leveraging technology and design to create solutions for the community.',
+    social: "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.instagram.com/zeng_lai/\">@zeng_lai</a>," +
+        "&nbsp; " +
+        "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.linkedin.com/in/zengconglai/\">LinkedIn</a>"
   },
   {
     name: 'Jolie Wang',
     role: 'Software Development',
     imageUrl:require('@/assets/members/jolie.png'),
-    bio:'Jolie Wang is a highly skilled full-stack developer and product designer with a fervent passion for crafting exceptional user experiences. As a freelance illustrator and web content consultant, Jolie leverages her comprehensive expertise in programming, art, and design to help clients bring their innovative ideas to fruition. Dedicated to promoting the advancement of women in technology, Jolie actively employs her talents to support and empower her female peers within the industry.'
+    bio:'Jolie Wang is a highly skilled full-stack developer and product designer with a fervent passion for crafting exceptional user experiences. As a freelance illustrator and web content consultant, Jolie leverages her comprehensive expertise in programming, art, and design to help clients bring their innovative ideas to fruition. Dedicated to promoting the advancement of women in technology, Jolie actively employs her talents to support and empower her female peers within the industry.',
+    social: "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.linkedin.com/in/jolie-wang/\">LinkedIn</a>"
   },
   {
     name: 'Chevonne Xue',
     role: 'Visual Design',
     imageUrl:require('@/assets/members/chevonne.png'),
-    bio:'Chevonne is a sociologist-designer focusing on user experience and product development. Her background in sociology, communication studies, and design background allows her to create aesthetic and feasible design solutions that link revenue to creating social benefits.'
+    bio:'Chevonne is a sociologist-designer focusing on user experience and product development. Her background in sociology, communication studies, and design background allows her to create aesthetic and feasible design solutions that link revenue to creating social benefits.',
+    social: "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.instagram.com/chevonne.x/\">@chevoonne.x</a>," +
+        "&nbsp; " +
+        "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.linkedin.com/in/chevonnex/\">LinkedIn</a>"
   },
   {
     name: 'Emily Gong',
     role: 'Community Partnerships',
     imageUrl:require('@/assets/members/emily.png'),
-    bio:'Emily Gong (she/her/they) is a Chinese Canadian artist, researcher, and entrepreneur. Emily holds a Bachelor of Fine Arts Honours degree from Queen’s University and a Master of Science degree from the University of Oxford. Her masters research is focused on the comparative studies of the global art markets from the perspective of empowering artists in this system. Emily has written for the Barbican Centre and exhibited in the Shenzhen-Hong Kong Biennale of Urbanism.'
+    bio:'Emily Gong (she/her/they) is a Chinese Canadian artist, researcher, and entrepreneur. Emily holds a Bachelor of Fine Arts Honours degree from Queen’s University and a Master of Science degree from the University of Oxford. Her masters research is focused on the comparative studies of the global art markets from the perspective of empowering artists in this system. Emily has written for the Barbican Centre and exhibited in the Shenzhen-Hong Kong Biennale of Urbanism.',
+    social: "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.instagram.com/g_em_/\">@g_em_</a>," +
+        "&nbsp; " +
+        "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.linkedin.com/in/emily-gong-0a69b1110/\">LinkedIn</a>"
   },
   {
     name: 'Erin Rooney',
     role: 'Editor',
     imageUrl:require('@/assets/members/erin.png'),
-    bio:'Erin is an information specialist with an MI from the University of Toronto. She has a passion for creative writing, culture, and the arts. She spent her formative years abroad, primarily in Asia, where she gained a lasting appreciation for Asian culture. Erin holds a BA in English Literature and is fascinated by the role that storytelling plays within culture and society. She is an advocate for celebrating cultural diversity, joining AACT to promote cultural awareness through the arts.'
+    bio:'Erin is an information specialist with an MI from the University of Toronto. She has a passion for creative writing, culture, and the arts. She spent her formative years abroad, primarily in Asia, where she gained a lasting appreciation for Asian culture. Erin holds a BA in English Literature and is fascinated by the role that storytelling plays within culture and society. She is an advocate for celebrating cultural diversity, joining AACT to promote cultural awareness through the arts.',
+    social: "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.linkedin.com/in/erin-rooney-7b8057191/\">LinkedIn</a>"
   },
   {
     name: 'Michael Wang',
     role: 'Operations',
     imageUrl:require('@/assets/members/michael.png'),
-    bio:'Michael Wang (he/him/his), Ph.D., is a vision scientist and tech enthusiast. His deep fascination about how humans perceive the world around them has led him to explore how personal and lived experiences are represented in various forms of art. To this end, Michael applies his knowledge in scientific research and modern technology to collaborate with local artists in Toronto to transform the creation and exhibition of arts.'
+    bio:'Michael Wang (he/him/his), Ph.D., is a vision scientist and tech enthusiast. His deep fascination about how humans perceive the world around them has led him to explore how personal and lived experiences are represented in various forms of art. To this end, Michael applies his knowledge in scientific research and modern technology to collaborate with local artists in Toronto to transform the creation and exhibition of arts.',
+    social: "<a class=\"font-medium text-bitten/80 hover:text-bitten\" href=\"https://www.linkedin.com/in/xiaoye-michael-w-7a246849/\">LinkedIn</a>"
   },
 
   //more members here
