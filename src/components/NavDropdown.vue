fe27c9e65d27438cacbf964b01d3e72b6c870077
callmejojow <73418982+callmejojow@users.noreply.github.com>
2023-05-27 01:05:45 -0400

    Update NavDropdown.vue

--- src/components/NavDropdown.vue
+++ src/components/NavDropdown.vue
@@ -59,6 +59,16 @@
                 About Us
               </a>
             </MenuItem>
+            <MenuItem v-slot="{ active }">
+              <a
+                :class="[
+                  active ? 'bg-stone-600/40 text-ivory/80' : 'text-stone-800',
+                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                ]" href="https://pages.donately.com/asianartsandculturetrust/form/frm_0f49dac5877d"
+              >
+                Donate
+              </a>
+            </MenuItem>
           </div>
         </MenuItems>
       </transition>
